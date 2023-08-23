7b75a6f136711dd7c39d73c7dd9da86164156205
giacomobranchi <branchigiacomo28@gmail.com>
2023-08-23 15:49:51 +0200

    add script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,26 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 */
+
+let age = Number(prompt("Inserisci la tua età"))
+let distance = Number(prompt("Inserisci i kilometri che devi percorrere"))
+
+console.log(age, distance);
+
+let price
+
+if (age<18) {
+    price = distance * 0.21 * 0.8
+} else if (age>65) {
+    price = distance * 0.21 * 0.6
+} else {
+    price = distance * 0.21
+}
+
+console.log(price);
+
+let actualPrice = price.toFixed(2)
+
+console.log(actualPrice);
+
+document.getElementById("ticket").innerHTML = "Il prezzo del tuo biglietto è di " + actualPrice + " Euro";
